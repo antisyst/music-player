94e2ae9e098a7dfab805af4509003bae5f73ea34
antisyst <redmiramazan@gmail.com>
2023-04-23 00:58:12 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import CardProfile from './music';
+
+function App() {
+
+  return (
+    <>
+    <CardProfile/>
+    <h3 className='creator-content'>Developed by <a href="https://rmzn.netlify.app" target='_blank'>Ramazan Azimli</a></h3>
+    </>
+  )
+}
+
+export default App
